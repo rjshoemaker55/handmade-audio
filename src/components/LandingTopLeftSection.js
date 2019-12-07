6544a9577d3e9c6ff443a5e68f6b1c5e7b93c8eb
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-12-06 20:46:28 -0500

    began styling

--- src/components/LandingTopLeftSection.js
+++ src/components/LandingTopLeftSection.js
@@ -3,8 +3,8 @@ import React from 'react';
 const LandingTopLeftSection = () => {
   return (
     <div id='tl-section-wrapper'>
-      <div className='top-section-header'>Who we are...</div>
-      <div className='top-section-body'>
+      <div id='top-left-section-header'>Who we are...</div>
+      <div id='tl-section-body'>
         Handmade Audio is a family high end audio retailer based in Flemington
         New Jersey.
       </div>
--- src/components/LandingTopRightSection.js
+++ src/components/LandingTopRightSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LandingTopRightSection = () => {
   return (
     <div id='tr-section-wrapper'>
-      <div className='top-section-header'>News</div>
+      <div id='top-right-section-header'>News</div>
       <div className='top-section-body'>Body</div>
     </div>
   );
