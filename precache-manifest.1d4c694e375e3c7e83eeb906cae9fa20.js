b7b2b30a9c686172e22c468ef41502d3dfb21db1
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-12-08 13:56:32 -0500

    Updates

---
+++ precache-manifest.1d4c694e375e3c7e83eeb906cae9fa20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f653d8387d59f422efe33504d0bac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eefa48349575f0158dc",
+    "url": "/static/css/main.ea1dd6f8.chunk.css"
+  },
+  {
+    "revision": "82b1fe26f5b10c9bf411",
+    "url": "/static/js/2.f5f91fe1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f5f91fe1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eefa48349575f0158dc",
+    "url": "/static/js/main.0fb1ee0f.chunk.js"
+  },
+  {
+    "revision": "a089c4487d7cefe6a9af",
+    "url": "/static/js/runtime-main.f6dd6132.js"
+  },
+  {
+    "revision": "e9017bdb4715fa040450e5b3ad818eb3",
+    "url": "/static/media/Speakers_on_shelf.e9017bdb.jpg"
+  }
+]);
